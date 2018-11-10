07e0bc15082c1aa2c59fb3c199ac34023db7a646
imanshafiei <imanshafiei@ymail.com>
2018-11-11 00:27:39 +0330

    Improvment and bug fixes

--- cmanager/models.py
+++ cmanager/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 
 class User(models.Model):
     fisrt_name = models.CharField(max_length=30, null=False)
@@ -18,6 +18,8 @@ class Game(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     start_time = models.TimeField(null=False)
     end_time = models.TimeField(null=True, default="00:00")
+    numbers = models.IntegerField(null=False)
+    add_date = models.DateField(default=datetime.datetime.now().date(), null=True)
 
     def __str__(self):
         return str(self.user.card_number)
--- cmanager/views.py
+++ cmanager/views.py
@@ -2,17 +2,46 @@ from django.shortcuts import render
 from cmanager.models import *
 import datetime
 
+
 def addgame(request):
     if request.method == "POST":
-        card_number = request.POST['card']
-        user_select = User.objects.get(card_number=card_number)
-        current_game = Game.objects.filter(user=user_select, end_time="00:00:00")
-        if len(current_game):
-            print(datetime.datetime.now().time())
-            current_game[0].end_time = datetime.datetime.now().time()
-            current_game[0].save()
-        else:
-            new_game = Game(user=user_select, start_time=datetime.datetime.now().time())
-            new_game.save()
+        try:
+            if request.POST['addorstop']:
+                card_number = request.POST['card']
+                nums = request.POST['numbers']
+                user_select = User.objects.get(card_number=card_number)
+                current_game = Game.objects.filter(user=user_select, end_time="00:00:00")
+                if len(current_game):
+                    current_game[0].end_time = datetime.datetime.now().time()
+                    current_game[0].save()
+                else:
+                    new_game = Game(user=user_select, start_time=datetime.datetime.now().time(), numbers=nums)
+                    new_game.save()
+        except Exception as e:
+            print(e)
+            try:
+                if request.POST['adduser'] == "true":
+                    f_name = request.POST['fname']
+                    l_name = request.POST['lname']
+                    phone = request.POST['phone']
+                    year = request.POST['year']
+                    month = request.POST['month']
+                    day = request.POST['day']
+                    card = request.POST['card']
+                    new_user = User(fisrt_name=f_name, last_name=l_name, phone=phone, year_of_birth=year, month_of_birth=month, day_of_birth=day, card_number=card)
+                    new_user.save()
+            except Exception as e:
+                print(e)
+                print("Nothing!")
+    today_users = Game.objects.filter(add_date=datetime.datetime.now().date())
+    users_list = []
+    for e in today_users:
+        start = e.start_time
+        end = e.end_time
+        timedelta_start = datetime.timedelta(hours=start.hour, minutes=start.minute, seconds=start.second)
+        timedelta_end = datetime.timedelta(hours=end.hour, minutes=end.minute, seconds=end.second)
+        t = timedelta_end - timedelta_start
+        point = int(t.total_seconds() / 225)
+        users_list.append({'user_obj': e, "price": point * 500, 'point': point})
 
-    return render(request, 'addgame.html')
+    return render(request, 'addgame.html', {"user_data": users_list})
