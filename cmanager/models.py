6335a3c8adcb6bf536fe451153c98f936f2659ef
imanshafiei <imanshafiei@ymail.com>
2018-11-23 20:55:44 +0330

    Improvment and bug fixes

--- cmanager/models.py
+++ cmanager/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-import datetime
+
+
+INTRO_CHOICES = (
+    ('search', 'جست‌وجوی اینترنت'),
+    ('friends', 'معرفی دوستان'),
+    ('instagram', 'اینستاگرام'),
+    ('roomiz', 'سایت رومیز'),
+    ('events', 'شرکت در رویدادها'),
+    ('other', 'سایر'),
+)
+
 
 class User(models.Model):
     fisrt_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=50, null=False)
-    card_number = models.CharField(max_length=20 ,null=False)
+    card_number = models.CharField(max_length=20, null=False)
     phone = models.CharField(max_length=15, null=False)
+    intro = models.CharField(max_length=50, choices=INTRO_CHOICES, default='other')
     year_of_birth = models.IntegerField(null=False)
     month_of_birth = models.IntegerField(null=False)
     day_of_birth = models.IntegerField(null=False)
@@ -19,7 +30,7 @@ class Game(models.Model):
     start_time = models.TimeField(null=False)
     end_time = models.TimeField(null=True, default="00:00")
     numbers = models.IntegerField(null=False)
-    add_date = models.DateField(default=datetime.datetime.now().date(), null=True)
+    add_date = models.DateField(null=False)
 
     def __str__(self):
         return str(self.user.card_number)
--- cmanager/views.py
+++ cmanager/views.py
@@ -15,7 +15,8 @@ def addgame(request):
                     current_game[0].end_time = datetime.datetime.now().time()
                     current_game[0].save()
                 else:
-                    new_game = Game(user=user_select, start_time=datetime.datetime.now().time(), numbers=nums)
+                    new_game = Game(user=user_select, start_time=datetime.datetime.now().time(), numbers=nums,
+                                    add_date=datetime.datetime.now().date())
                     new_game.save()
         except Exception as e:
             print(e)
@@ -24,11 +25,13 @@ def addgame(request):
                     f_name = request.POST['fname']
                     l_name = request.POST['lname']
                     phone = request.POST['phone']
+                    intro = request.POST['intro']
                     year = request.POST['year']
                     month = request.POST['month']
                     day = request.POST['day']
                     card = request.POST['card']
-                    new_user = User(fisrt_name=f_name, last_name=l_name, phone=phone, year_of_birth=year, month_of_birth=month, day_of_birth=day, card_number=card)
+                    new_user = User(fisrt_name=f_name, last_name=l_name, phone=phone, year_of_birth=year,
+                                    month_of_birth=month, day_of_birth=day, card_number=card, intro=intro)
                     new_user.save()
             except Exception as e:
                 print(e)
@@ -36,12 +39,16 @@ def addgame(request):
     today_users = Game.objects.filter(add_date=datetime.datetime.now().date())
     users_list = []
     for e in today_users:
+        point = 0
         start = e.start_time
         end = e.end_time
         timedelta_start = datetime.timedelta(hours=start.hour, minutes=start.minute, seconds=start.second)
-        timedelta_end = datetime.timedelta(hours=end.hour, minutes=end.minute, seconds=end.second)
-        t = timedelta_end - timedelta_start
-        point = int(t.total_seconds() / 225)
-        users_list.append({'user_obj': e, "price": point * 500, 'point': point})
+        if str(end) != "00:00:00":
+            timedelta_end = datetime.timedelta(hours=end.hour, minutes=end.minute, seconds=end.second)
+            t = timedelta_end - timedelta_start
+            point = int(t.total_seconds() / 225)
+            users_list.append({'user_obj': e, "price": point * 500 * e.numbers, 'point': point * e.numbers, "end": 1})
+        else:
+            users_list.append({'user_obj': e, "price": point * 500 * e.numbers, 'point': point * e.numbers, "end": 0})
 
     return render(request, 'addgame.html', {"user_data": users_list})
